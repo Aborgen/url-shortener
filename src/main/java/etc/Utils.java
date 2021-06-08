740b0c3cfe771e0ac31c242f88b7ee873d1cca0f
Aborgen <alexanderborgen@gmail.com>
2021-06-08 15:11:40 -0700

    Create method that converts from a digit to base64

--- src/main/java/etc/Utils.java
+++ src/main/java/etc/Utils.java
@@ -4,6 +4,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Utils {
+  static int BASE = 64;
+  static String base64Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-";
   public static boolean validateUrl(String url) {
     try {
       new URL(url);
@@ -12,5 +14,18 @@ public class Utils {
     catch (MalformedURLException e) {
       return false;
     }
+  }
+
+  public static String convertDigit(long n) {
+    StringBuilder result = new StringBuilder();
+    do {
+      int remainder = (int) (n % BASE);
+      n /= BASE;
+      result.append(base64Chars.charAt(remainder));
+    }
+    while (n > 0);
+
+    result.reverse();
+    return result.toString();
   }
 }
