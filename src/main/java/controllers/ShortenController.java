eef43c43209f8a5f06c3b98048fbdea06fb37a76
Aborgen <alexanderborgen@gmail.com>
2021-06-08 16:42:25 -0700

    Update ShortenController to provide the user with helpful messages when something goes wrong when attemping to add entries to the database

--- src/main/java/controllers/ShortenController.java
+++ src/main/java/controllers/ShortenController.java
@@ -14,23 +14,34 @@ public class ShortenController {
   @Inject
   DatabaseActions db;
 
+  String flashErrorMessage;
+
   public Result index(@Param("url") String url, FlashScope flashScope) {
     String shortenedUrl = shorten(url);
     if (shortenedUrl == null) {
-      String s = String.format("Invalid URL: '%s'", url);
-      flashScope.error(s);
-      return Results.redirect("/");
+      return redirectWithFlashError(flashScope);
     }
 
     return Results.html().render("url", shortenedUrl);
   }
 
+  private Result redirectWithFlashError(FlashScope flashScope) {
+    flashScope.error(flashErrorMessage);
+    return Results.redirect("/");
+  }
+
   private String shorten(String url) {
     if (!Utils.validateUrl(url)) {
+      flashErrorMessage = String.format("Invalid URL: '%s'", url);
       return null;
     }
 
-    db.setEntry("0", url);
+    boolean result = db.setEntry("0", url);
+    if (!result) {
+      flashErrorMessage = String.format("There was an issue generating your shortened url. Please try again.", url);
+      return null;
+    }
+
     return url;
   }
 }
