f944fb55422d451bf4befc97b1ae674878721313
Aborgen <alexanderborgen@gmail.com>
2021-06-25 15:44:13 -0700

    Refactor FileOperations to revolve around a synchronized map of File objects
    This change is supposed to remove any ambiguity around multiple clients trying to access from and mutate the same collection of combination files

--- src/main/java/etc/FileOperations.java
+++ src/main/java/etc/FileOperations.java
@@ -9,10 +9,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FileOperations {
   final static File combinationsDir = new File("./combinations");
+  static Map<Integer, File> syncFiles;
   DatabaseActions db;
 
   public FileOperations(DatabaseActions db) {
@@ -29,18 +33,25 @@ public class FileOperations {
       e.printStackTrace();
       return;
     }
+    int digitCount = db.getUrlCardinality();
+    File[] files = getSubCombinationDir(digitCount).listFiles();
+    HashMap<Integer, File> map = new HashMap<>();
+    for (int i = 0; i < files.length; ++i) {
+      map.put(i, files[i]);
+    }
+
+    syncFiles = Collections.synchronizedMap(map);
   }
 
   public String getAvailableUrl(int digitCount) {
-    File d = getSubCombinationDir(digitCount);
-    File[] files = d.listFiles();
-    if (files.length == 0) {
+    if (syncFiles.size() == 0) {
       return null;
     }
 
     ThreadLocalRandom rand = ThreadLocalRandom.current();
-    File file = files[rand.nextInt(files.length)];
-    String result = "";
+    int fileIndex = rand.nextInt(syncFiles.size());
+    File file = syncFiles.get(fileIndex);
+    String result;
     try (
       FileChannel fileChannel = FileChannel.open(file.toPath(), StandardOpenOption.READ);
     ) {
@@ -52,7 +63,12 @@ public class FileOperations {
       }
       else if (fileChannel.size() == tokenLength) {
         fileChannel.read(buf, 0);
-        file.delete();
+        if (file.delete()) {
+          syncFiles.remove(fileIndex);
+        }
+        else {
+          throw new IOException(String.format("Unable to delete file: %s", file.getAbsolutePath()));
+        }
       }
       else {
         long position = rand.nextLong(fileChannel.size() / tokenLength) * tokenLength;
